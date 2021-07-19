32fa6127f86dab18838ff6a2935c82502b3a6f02
Angel Mejia <amejia.foo@gmail.com>
2021-07-18 22:32:48 -0500

    Update: Class Code
    Modified things that don't work anymore keeping the approach.

--- bin/main.dart
+++ bin/main.dart
@@ -3,9 +3,13 @@ import 'dart:async';
 
 main(List<String> arguments) {
 
+  // Display message on terminal
   stdout.write('What is your name?\r\n');
-  String name = stdin.readLineSync();
 
-  name.isEmpty ? stderr.write('Name is empty') : stdout.write('Hello ${name} :) \r\n');
+  // Store entered value in name
+  String? name = stdin.readLineSync()!;
+
+  // Ternary Operator
+  name.isEmpty ? stderr.write('Error: Name is empty, try again.') : stdout.write('Hello, $name.\r\n');
 
 }
